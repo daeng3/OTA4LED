8caa444d4a78a79d39f48081f2f0448dffb4d438
firdan <firdangusmara@gmail.com>
2026-02-16 12:50:17 +0700

    uplaod firmware

--- 4ledOTA.ino
+++ 4ledOTA.ino
@@ -1,62 +1,73 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <WiFiClientSecure.h>
 #include <HTTPUpdate.h>
 
-const char* ssid = "hadahade";
+// --- KONFIGURASI WIFI ---
+const char* ssid     = "hadahade";
 const char* password = "levion1234";
 
-// URL file biner di GitHub (Gunakan link "Raw")
-String binURL = "https://raw.githubusercontent.com/username/repo/main/firmware.bin";
+// --- KONFIGURASI GITHUB OTA ---
+// Gunakan link RAW (harus diawali raw.githubusercontent.com)
+const String firmwareURL = "https://raw.githubusercontent.com/daeng3/OTA4LED/main/4ledOTA.ino.bin";
 
+// Versi firmware saat ini (Tingkatkan angka ini setiap kali mau update)
+const int currentVersion = 1;
+
+// Pin LED
 int leds[] = {18, 19, 21, 22};
-int currentVersion = 1; // Ubah jadi 2 saat upload ke GitHub
 
 void setup() {
   Serial.begin(115200);
-  for(int i=0; i<4; i++) pinMode(leds[i], OUTPUT);
+  
+  // Inisialisasi LED
+  for(int i=0; i<4; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
 
+  // Koneksi WiFi
+  Serial.print("Connecting to WiFi");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   
-  Serial.println("\nWiFi Connected. Version: " + String(currentVersion));
+  Serial.println("\nWiFi Connected!");
+  Serial.print("Current Firmware Version: ");
+  Serial.println(currentVersion);
+
+  // Jalankan pengecekan update saat pertama kali menyala
   check_for_update();
 }
 
 void loop() {
-  // Efek Running LED sebagai indikator firmware aktif
+  // Efek Running LED (Indikator firmware aktif)
   for(int i=0; i<4; i++) {
     digitalWrite(leds[i], HIGH);
-    delay(100);
+    delay(200);
     digitalWrite(leds[i], LOW);
   }
 }
 
 void check_for_update() {
+  Serial.println("Checking for firmware updates...");
+  
   WiFiClientSecure client;
-  client.setInsecure(); // Mengabaikan validasi SSL untuk kemudahan testing
+  client.setInsecure(); // Mengabaikan validasi SSL agar mudah untuk testing
 
-  t_httpUpdate_return ret = httpUpdate.update(client, binURL);
+  // Proses update
+  t_httpUpdate_return ret = httpUpdate.update(client, firmwareURL);
 
   switch (ret) {
     case HTTP_UPDATE_FAILED:
-      Serial.printf("Update Gagal: (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+      Serial.printf("Update Gagal! Error (%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
       break;
     case HTTP_UPDATE_NO_UPDATES:
-      Serial.println("Tidak ada update terbaru.");
+      Serial.println("Tidak ada update biner baru.");
       break;
     case HTTP_UPDATE_OK:
-      Serial.println("Update Berhasil!");
+      Serial.println("Update Berhasil! Rebooting...");
       break;
   }
-}void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
